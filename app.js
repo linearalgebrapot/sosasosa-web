c3412a12af603738d60de45f5ad9a90231ec5530
Bammer <jhcse5189@gmail.com>
2020-03-29 04:26:03 +0900

    test. express-subdomain in main

--- app.js
+++ app.js
@@ -9,20 +9,36 @@
 
 // import express modules
 const express = require('express'),
+      subdomain = require('express-subdomain'),
       http = require('http');
 
 
 // import to provide sub-domains
-const vhost = require('vhost'),
-      pingping = require('./sub/pingping/app');
+// const vhost = require('vhost'),
+//       pingping = require('./sub/pingping/app');
 
 // create express object
 var app = express();
 // set the port as attr. of app obj.
 app.set('port', process.env.PORT || 3000);
 
-app.use(vhost('pingping.sosasosa.dev', pingping));
+var router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send('pingping!');
+});
+
+router.get('/admin', (req, res) => {
+    res.json([
+        {name: "Bammer"}
+    ]);
+});
+
+app.use(subdomain('pingping', router));
+
+app.get('/', (req, res) => {
+    res.send('Homepage!');
+});
 /*
 // create main(non-sub-domained) router object
 var router = express.Router();
@@ -35,7 +51,7 @@ router.route('/').get((req, res) => {
 });
 */
 
-app.use(express.static('static'));
+//app.use(express.static('static'));
 
 // register router objs. to app obj.
 //app.use('/', router);
